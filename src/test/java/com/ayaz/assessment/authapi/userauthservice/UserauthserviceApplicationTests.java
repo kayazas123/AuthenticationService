06560c30ebc2e498fe17cb35da8e38a72451bdcc
kayazas@gmail.com <Xdr56yhN>
2019-03-09 20:36:19 +0200

    Initial commit

---
+++ src/test/java/com/ayaz/assessment/authapi/userauthservice/UserauthserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ayaz.assessment.authapi.userauthservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserauthserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
